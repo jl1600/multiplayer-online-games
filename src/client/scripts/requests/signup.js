a5f974ce91fa5fa38f8c2ebadda4026430476fa3
ChingChang9 <chingtheprogrammer@gmail.com>
2021-08-11 09:49:14 -0400

    fix sign up bug

--- src/client/scripts/requests/signup.js
+++ src/client/scripts/requests/signup.js
@@ -1,22 +1,3 @@
-const xhr = new XMLHttpRequest();
-const url = "http://localhost:8000/register";
-
-function signup(username, password, confirmPassword) {
-	if (!username || !password) return;
-	if (!checkPassword(password, confirmPassword)) return;
-
-	xhr.open("POST", url);
-	xhr.setRequestHeader("Content-Type", "application/json");
-
-	xhr.onreadystatechange = () => {
-		if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-			console.log(xhr.response);
-		}
-	};
-
-	xhr.send(JSON.stringify({ username, password }));
-}
-
 function checkPassword() {
 	if (document.getElementById("password").value !== document.getElementById("confirmPassword").value) {
 		document.getElementById("errorMessage").innerHTML = "Passwords don't match!";
@@ -28,8 +9,8 @@ function checkPassword() {
 	}
 }
 function signup(username, password, confirmPassword, userType) {
-	if (!username || !password) return;
-	if (!checkPassword(password, confirmPassword)) return;
+	if (!username || !password) return false;
+	if (!checkPassword(password, confirmPassword)) return false;
 
 	xhr.open("POST", "http://localhost:8000/register");
 	xhr.setRequestHeader("Content-Type", "application/json");
