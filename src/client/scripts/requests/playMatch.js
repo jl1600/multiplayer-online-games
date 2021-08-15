fd731742ee464584ce852619964fb3196a5ec722
ChingChang9 <chingtheprogrammer@gmail.com>
2021-08-15 02:21:57 -0400

    trigger buttons on enter

--- src/client/scripts/requests/playMatch.js
+++ src/client/scripts/requests/playMatch.js
@@ -7,7 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
 	if (!!matchId) joinMatch(matchId);
 	else document.getElementById("start-button").hidden = false;
 	createSocket();
-}, false);
+}, false);g
+document.getElementById("match-input").addEventListener("keyup", event => {
+    if (event.keyCode === 13) sendInput();
+});
 
 function joinMatch(matchID) {
 	xhr.open("POST", "http://localhost:8000/game/join-match");
@@ -51,10 +54,10 @@ function displayStats(data) {
 	}
 }
 
-function sendInput(input) {
+function sendInput() {
 	serverSocket.send(JSON.stringify({
 		sysCommand: "",
-		gameMove: input
+		gameMove: document.getElementsByName("matchInput")[0].value
 	}));
 	document.getElementById("match-input").value = "";
     document.getElementById("match-input").focus();
