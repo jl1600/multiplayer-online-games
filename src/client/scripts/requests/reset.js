124cfe88ade4631c37268670fa5d02feafb33af1
Shivneel-Mistry <shivneel.mistry@mail.utoronto.ca>
2021-08-16 11:17:30 -0400

    Fixed bugs Reset.html and Rest.js

--- src/client/scripts/requests/reset.js
+++ src/client/scripts/requests/reset.js
@@ -2,7 +2,6 @@ if (typeof xhr === "undefined") xhr = new XMLHttpRequest();
 
 function reset(username, email) {
 	if (!username || !email) return;
-	if (!checkValidEmail) return;
 	xhr.open("POST", "http://localhost:8080/user/forgot-password");
 	xhr.onreadystatechange = () => {
     		if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
@@ -15,13 +14,6 @@ function reset(username, email) {
     	};
 
 	xhr.send(JSON.stringify({username, email}));
-    // console.log("redirecting to login.html")
-	// redirect();
-}
-
-function checkValidEmail(email){
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
 }
 
 function redirect(){
