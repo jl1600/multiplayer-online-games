426b9fb6d1f92cda2875a88b037df9f2f5cdc08d
Shivneel-Mistry <shivneel.mistry@mail.utoronto.ca>
2021-08-16 18:52:42 -0400

    small code changes

--- src/client/scripts/requests/reset.js
+++ src/client/scripts/requests/reset.js
@@ -1,21 +1,18 @@
 if (typeof xhr === "undefined") xhr = new XMLHttpRequest();
 
-function reset(username, email) {
-	if (!username || !email) return;
+function forgotPass() {
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    console.log(username, email)
 	xhr.open("POST", "http://localhost:8080/user/forgot-password");
+	console.log("here", JSON.parse(xhr.response), username, email);
 	xhr.onreadystatechange = () => {
-    		if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-    			window.location = "http://localhost:8080/pages/login.html";
-    		} else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 204) {
-    		    alert("Expected an email")
-    		} else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 403) {
-                alert("Invalid username or Email")
-             }
-    	};
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 204) {
+    	    console.log("trying to reset");
+    	} else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 403) {
+            alert("Invalid username or Email");
+        }
+    };
 
 	xhr.send(JSON.stringify({username, email}));
-}
-
-function redirect(){
-    window.location = "http://localhost:8080/pages/login.html"
 }
